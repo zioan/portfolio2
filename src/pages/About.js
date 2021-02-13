46d9c69a3ed47f75755bc46369796b0fef888969
zioan <zaharia.ioan@gmail.com>
2021-02-13 15:11:15 +0100

    init final portfolio

---
+++ src/pages/About.js
@@ -0,0 +1,91 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import ProgressiveImage from "react-progressive-image";
+import Layout from "../components/Layout";
+import Sectiontitle from "../components/Sectiontitle";
+
+function About() {
+  const [information, setInformation] = useState("");
+
+  useEffect(() => {
+    axios.get("/api/information").then((response) => {
+      setInformation(response.data);
+    });
+  }, []);
+
+  return (
+    <Layout>
+      <div className="mi-about-area mi-section mi-padding-top">
+        <div className="container">
+          <Sectiontitle title="About Me" />
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="mi-about-image">
+                <ProgressiveImage
+                  src={information.aboutImage}
+                  placeholder="/images/about-image-placeholder.png"
+                >
+                  {(src) => <img src={src} alt="aboutimage" />}
+                </ProgressiveImage>
+                <span className="mi-about-image-icon"></span>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="mi-about-content">
+                <h3>
+                  I am <span className="color-theme">{information.name}</span>
+                </h3>
+                <p>
+                  I am a frontend web developer. I can provide clean code and
+                  pixel perfect design.
+                </p>
+                <ul>
+                  {!information.name ? null : (
+                    <li>
+                      <b>Full Name</b> {information.name}
+                    </li>
+                  )}
+                  {!information.age ? null : (
+                    <li>
+                      <b>Age</b> {information.age} Years
+                    </li>
+                  )}
+                  {!information.phone ? null : (
+                    <li>
+                      <b>Phone</b> {information.phone}
+                    </li>
+                  )}
+                  {!information.nationality ? null : (
+                    <li>
+                      <b>Nationality</b> {information.nationality}
+                    </li>
+                  )}
+                  {!information.language ? null : (
+                    <li>
+                      <b>Languages</b> {information.language}
+                    </li>
+                  )}
+                  {!information.email ? null : (
+                    <li>
+                      <b>Email</b> {information.email}
+                    </li>
+                  )}
+                  {!information.address ? null : (
+                    <li>
+                      <b>Address</b> {information.address}
+                    </li>
+                  )}
+                </ul>
+                <a href={information.cvfile} className="mi-button">
+                  Download CV
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default About;
